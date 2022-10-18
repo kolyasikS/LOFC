9a300418ecfd3109eea938f862fbc69f3f93d327
kolyasik <o687801077@gmail.com>
2022-10-18 11:38:56 +0300

    add branch - 'cerating club'

---
+++ LeageOfChampoins/BLL/Entities/Owner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Entities
+{
+    public class Owner :IdKey
+    {
+        string Name { get; set; }
+        string? Surname { get; set; }
+        string Nation { get; set; }
+        int Age { get; set; }
+        double Capital { get; set; }
+        [ForeignKey "Club"]
+        public int CLubId { get; set; }
+        public Club Club { get; set; } 
+    }
+}
